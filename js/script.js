762f55606cc191efab893181c033107d153b9c28
Pedro H. L. Queiroz <123014853+PedroHLZ@users.noreply.github.com>
2023-05-29 19:34:07 -0300

    update bug

--- js/script.js
+++ js/script.js
@@ -109,11 +109,3 @@ window.addEventListener('scroll', () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 });
-//    modal    //
-function openModal(modalId) {
-    document.getElementById(modalId).style.display = "block";
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
-}
